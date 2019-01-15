e5dbab56d6004acf898c76d25f49e352a585b8ff
Morgan <nicol.morgan@outlook.fr>
2019-01-15 15:07:11 +0100

    script active leftmenu + logout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,10 @@
 $(function () {
+
+    // ########## LEFT PANNEL ##########
+    $('.leftpannel .project').on('click', function (e) {
+        if (!$(this).hasClass('active')) {
+            $('.leftpannel .project').removeClass('active')
+            $(this).addClass('active')
+        }
+    });
 });
