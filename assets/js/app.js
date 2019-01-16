584e65b9337b3185e4a48cb355ae4a1274825ffa
Morgan <nicol.morgan@outlook.fr>
2019-01-16 16:55:22 +0100

    Ajout de la vue client, résolution du pb de app.js sur menu, template de la swal pour client & projet nouveaux

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,21 @@
 $(function () {
-    // ################## MENU ##################
-    // trick pour poser le active du menu sur la bonne page
-    let origin = window.location.origin;
-    let url = window.location.pathname;
-    let urlSplitted = url.split('/')[3];
-    $('nav li a[href="' + urlSplitted + '"]').addClass('active');
+    // ########## DEFAULT GENERAL ##########
+    const swalMadera = Swal.mixin({
+        allowEnterKey: true,
+        background: '',
+        customClass: 'swal-modal',
+        showCancelButton: true,
+        cancelButtonText: 'Annuler',
+        confirmButtonText: 'Valider',
+        confirmButtonColor: '#535559',
+        cancelButtonColor: '#535559'
+    });
 
     // ########## LEFT PANNEL ##########
-    $('.leftpannel .project').on('click', function (e) {
+    // Gestion du Active
+    $('.leftpannel li').on('click', function (e) {
         if (!$(this).hasClass('active')) {
-            $('.leftpannel .project').removeClass('active')
+            $('.leftpannel .project').removeClass('active');
             $(this).addClass('active')
         }
     });
@@ -28,34 +34,40 @@ $(function () {
             $('.footer .date').html(date);
         }
         // FERMER LE PANNEL DU BAS
-        $('.close-data').on('click', function(e) {
+        $('.close-data').on('click', function (e) {
             $('.footer').hide();
         })
     });
 
-    const swalMadera = Swal.mixin({
-        allowEnterKey: true,
-        background: '',
-        customClass: 'swal-modal',
-        showCancelButton: true,
-        cancelButtonText: 'Annuler',
-        confirmButtonText: 'Valider',
-        confirmButtonColor: '#535559',
-        cancelButtonColor: '#535559'
-    });
-
-    $('.add-new-project').on('click', function(e) {
+    $('.add-new-project').on('click', function (e) {
         swalMadera({
-          title:'<div class="title">Ajouter un nouveau projet</div>',
-           html:"<form>" +
-           "<div><label for='title-proj'>Titre :</label><input type='text' id='title-proj' placeholder='Titre...'></div>" +
-           "<div><label for='desc-proj'>Description :</label><textarea type='text' id='desc-proj' placeholder='Description...'></textarea></div>" +
-           "</form>",
-       }).then(function(isConfirm) {
+            title: '<div class="title">Ajouter un nouveau projet</div>',
+            html: "<form>" +
+            "<div><label for='title-proj'>Titre :</label><input type='text' id='title-proj' placeholder='Titre...'></div>" +
+            "<div><label for='desc-proj'>Description :</label><textarea type='text' id='desc-proj' placeholder='Description...'></textarea></div>" +
+            "</form>",
+        }).then(function (isConfirm) {
             if (isConfirm.value === true) {
                 let titre = $('#title-proj').val();
                 let description = $('#desc-proj').val();
-                console.log('Valider Formulaire + save en database ici')
+                console.log(titre + ' ' + description + ' //// Valider Formulaire + save en database ici')
+            }
+        })
+    });
+
+    // ##########  CLIENT PAGE ##########
+    $('.add-new-client').on('click', function (e) {
+        swalMadera({
+            title: '<div class="title">Ajouter un nouveau client</div>',
+            html: "<form>" +
+            "<div><label for='nom-cli'>Nom :</label><input type='text' id='nom-cli' placeholder='Nom...'></div>" +
+            "<div><label for='prenom-cli'>Prenom :</label><textarea type='text' id='prenom-cli' placeholder='Prenom...'></textarea></div>" +
+            "</form>",
+        }).then(function (isConfirm) {
+            if (isConfirm.value === true) {
+                let nom = $('#nom-cli').val();
+                let prenom = $('#prenom-cli').val();
+                console.log(nom + ' ' + prenom + ' //// Valider Formulaire + save en database ici')
             }
         })
     });
--- assets/js/importMenu.js
+++ assets/js/importMenu.js
@@ -2,4 +2,5 @@ $(function () {
     let nav = $('.navbar');
     nav.load("shared/menu.html");
     nav.addClass(' navbar-light navbar-expand-lg fixed-top clean-navbar');
+
 });
