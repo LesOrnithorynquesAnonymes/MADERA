d05501bca7447651e60ab94522bfe50e9a5fbffc
destefani.yoann <ydestefa.destefani.yoann@gmail.com>
2019-03-06 12:05:06 +0100

    first module db commercial

---
+++ app/client/commercial_db.js
@@ -0,0 +1,14 @@
+var db = require('../db/connect_db.js');
+
+
+function byID(id){
+  db.query("SELECT * FROM commercial where commercial.com_id = " + db.escape(id), function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      for(var key in result) {
+        console.log(result[key].matricule);
+      }
+    });
+}
+
+byID(2);
--- app/db/connect_db.js
+++ app/db/connect_db.js
@@ -1,22 +1,8 @@
-var mysql = require('mysql');
-<<<<<<< HEAD
-console.log("Trying to connect..");
-=======
-
->>>>>>> 1f3803c0e77ce2673558362505d329d9ba27b455
-var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password :'',
-  database :'madera'
-});
-
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-connection.query("SELECT * FROM projet");
+module.exports = require('mysql').createConnection({
+    host : 'localhost',
+    user : 'root',
+    password :'',
+    database :'madera'
+  });
 
 //connection.end();
