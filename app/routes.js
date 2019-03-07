b1594705d36c25ca62205bc94e9f93c08a5b9c9d
JeyZu <jeremie.quatorze@gmail.com>
2019-03-07 14:53:36 +0100

    zdaz

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,8 @@
-
-router.post('/project', (req, res) => {
+router.get('/projects', (req, res) => {
   console.log(__dirname, req.params);
   Projet = require('../app/components/project/projet_db.js');
 
   projet = new Projet();
 
-  projet.set('id', 4);
-  projet.read();
-  console.log(projet.id);
+  res.json(projet.find('all'));
 });
--- assets/js/app.js
+++ assets/js/app.js
@@ -60,6 +60,18 @@ $(function () {
         }
     });
 
+    router.route('GET', '/projects', function(err, res) {
+      $('#projects').html('');
+      console.log(res, err);
+
+      if(!err){
+        for(var key in res) {
+          project = res.key;
+          $('#projects').append('<li id="project_' + project.id + '" class="project">' + project.name + '</li>');
+        }
+      }
+
+    });
 
     $('.add-new-project').on('click', function (e) {
         swalMadera({
